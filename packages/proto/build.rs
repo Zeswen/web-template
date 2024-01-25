6bc0a155c772654ce68b63584a867b9054ce4a4a
Javier López Navarro <me@zeswen.dev>
2024-01-25 18:07:27 +0000

    fix(proto): 🐛 Fix Rust application

--- packages/proto/build.rs
+++ packages/proto/build.rs
@@ -2,22 +2,31 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     tonic_build::configure()
-        .build_client(false)
-        .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
-        .out_dir("./authorization")
+        .build_server(false)
+        .out_dir("./google/api")
         .compile(
-            &["./authorization/authorization.proto"],
-            &["./authorization"],
+            &[
+                "./google/api/annotations.proto",
+                "./google/api/client.proto",
+                "./google/api/http.proto",
+                "./google/api/launch_stage.proto",
+            ],
+            &["./"],
         )?;
     tonic_build::configure()
         .build_client(false)
         .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .out_dir("./authorization")
+        .compile(&["./authorization/authorization.proto"], &["./"])?;
+    tonic_build::configure()
+        .build_client(false)
+        .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
         .out_dir("./payment")
-        .compile(&["./payment/payment.proto"], &["./payment"])?;
+        .compile(&["./payment/payment.proto"], &["./"])?;
     tonic_build::configure()
         .build_client(false)
         .type_attribute("User", "#[derive(serde::Deserialize, serde::Serialize)]")
         .out_dir("./product")
-        .compile(&["./product/product.proto"], &["./product"])?;
+        .compile(&["./product/product.proto"], &["./"])?;
     Ok(())
 }
